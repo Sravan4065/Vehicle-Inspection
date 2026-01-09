e03c9d50383a9527554d8b4d722c1d65718b9f7a
GDCIT-LAPT195\Dinesh_Bobbadhi <dinesh_bobbadhi@tecnics.com>
2026-01-09 15:07:17 +0530

    committing jan9 changes

--- VehicleInspection/controllers/mobile/frmTyresController/frmTyresController.js
+++ VehicleInspection/controllers/mobile/frmTyresController/frmTyresController.js
@@ -1,5 +1,142 @@
 define({ 
 
- //Type your controller code here 
+   onNavigate: function()
+  {
+    var self = this;
+    
+    this.view.preShow = this.onPreShow.bind(this);
+  
+   
+  },
+  
+  onPreShow: function()
+  {
+    var self = this;
+   toggleFooterIcons(this.view, "frmTyres"); 
+    
+   var i=1;
+
+while (i < 6) {
+  this.view["flxStar" + i].onClick =
+    this.callRate.bind(this);
+  i++;
+}
+    
+    this.createUIBox();
+  },
+  
+ callRate: function (context) {
+ 
+  var widgetId = context.id;
+
+  var rating = parseInt(widgetId.replace("flxStar", ""), 10);
+
+ 
+  for (var i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      this.view["imgStar" + i].src = "greenstar.png";
+    } else {
+      this.view["imgStar" + i].src = "ashstar.png";
+    }
+  }
+},
+  
+  createUIBox: function()
+  {
+    this.view.flxItems.removeAll();
+    
+     var flxItem = new voltmx.ui.FlexContainer({
+      
+      id: "flxItem",
+      height: "230dp",
+      width: "48%",
+      left: "0%",
+      top: "15dp",
+      skin: "sknFlxFFFFFFBorderCCCCCCRadius8px",
+      layoutType: voltmx.flex.FREE_FORM
+      
+      
+    }, {}, {})
+        
+             var flxImgItem = new voltmx.ui.FlexContainer({
+      
+      id: "flxImgItem",
+       height: "100dp",
+      width: "90%",
+      centerX: "50%",
+      skin: "sknFlxEFEFEFRadius5px",
+        top: "10dp",
+      layoutType: voltmx.flex.FREE_FORM,
+      clipBounds: true
+      
+      
+    }, {}, {})
+        
+      var imgItem = new voltmx.ui.Image2({
+      
+      id: "imgItem",
+      height: "100%",
+      width: "100%",
+      centerX: "50%",
+      centerY: "50%",
+      src: "defaulticon.png"
+
+    }, {imageScaleMode: constants.IMAGE_SCALE_MODE_FIT_TO_DIMENSIONS}, {})
+      
+      flxImgItem.add(imgItem);
+      
+      var lblItemName = new voltmx.ui.Label({
+      
+      id: "lblItemName",
+//       height: "100%",
+//       width: "48%",
+       centerX: "50%",
+       text: "Item",
+       top: "120dp",
+      skin: "sknLblDubai231f2020pxRegular",
+    }, {}, {});
+        
+           var flxClick = new voltmx.ui.FlexContainer({
+      
+      id: "flxClick",
+      height: "40dp",
+      width: "90%",
+      centerX: "50%",
+      bottom: "12dp",
+      skin: "sknFlxFFFFFFBorderCCCCCCRadius8px",
+      layoutType: voltmx.flex.FREE_FORM,
+//       onClick: this.openUploadPopup.bind(this,i)
+    }, {}, {})
+           
+            var imgCamera = new voltmx.ui.Image2({
+      
+      id: "imgCamera",
+      height: "15dp",
+      width: "15dp",
+      left: "25%",
+      centerY: "50%",
+      src: "cameraclick.png"
+
+    }, {}, {})
+           
+        var lblClick = new voltmx.ui.Label({
+      
+      id: "lblClick",
+//       height: "100%",
+//       width: "48%",
+        centerY: "50%",
+        left: "48%",
+       text: "Click",
+      skin: "sknLblDubai231f2018pxMedium",
+    }, {}, {});
+        
+        flxClick.add(imgCamera,lblClick); 
+        
+        flxItem.add(flxImgItem,lblItemName,flxClick);
+    
+    this.view.flxItems.add(flxItem);
+//     this.view.flxFields.removeAt(index);
+  }
+
 
  });
--- VehicleInspection/controllers/mobile/frmVehicledetailsInspectionTypeController/frmVehicledetailsInspectionTypeController.js
+++ VehicleInspection/controllers/mobile/frmVehicledetailsInspectionTypeController/frmVehicledetailsInspectionTypeController.js
@@ -1,24 +1,67 @@
 define({ 
 
- //Type your controller code here 
-onNavigate: function(){
-this.view.preShow =this.OnpreShow.bind(this);
-},
-  OnpreShow: function(){
-   
-var data = [
-    { lblData: "Vehicle ID : VH-10234" },
-    { lblData: "Auction Date : 25 Dec 2025" },
-    { lblData: "Base Price : AED 45,000" },
-    { lblData: "Current Bid : AED 52,000" },
-   { lblData: "Vehicle ID : VH-10234" },
-    { lblData: "Auction Date : 25 Dec 2025" },
-    { lblData: "Base Price : AED 45,000" },
-    { lblData: "Current Bid : AED 52,000" }
-];
+  onNavigate: function(){
+    this.view.preShow =this.onPreShow.bind(this);
+  },
+  onPreShow: function(){
+    toggleFooterIcons(this.view, "frmVehicledetailsInspectionType");
+    this.clearData();
+    this.setDataToSeg();  
+    for (let i = 1; i <= 16; i++) {
+  this.view["details" + i].flxArrow.onClick =
+    this.toggleDetails.bind(this);
+}
+  for (let i = 1; i <= 16; i++) {
+  this.view["details" + i].segVehicleDetails.onRowClick =
+    this.onRowClickAction.bind(this);
+}
+  },
 
-this.view.details.Segment0j5d01d22ad6d41.setData(data);
-
-   
+  toggleDetails: function (context) {
+  var detailsId = context.parent.parent.id;
+  var details = this.view[detailsId];
+    var transform = voltmx.ui.makeAffineTransform();
+    if (details.flxSegment.isVisible) {
+    details.flxSegment.isVisible = false;
+    transform.rotate(0);
+    details.imgarrow.transform = transform;
+  } else {
+    details.flxSegment.isVisible = true;
+    transform.rotate(180);
+    details.imgarrow.transform = transform;
   }
- });
+},
+
+  setDataToSeg: function()
+  {
+    var data = [
+      { lblData: "Vehicle ID : VH-10234" },
+      { lblData: "Auction Date : 25 Dec 2025" },
+      { lblData: "Base Price : AED 45,000" },
+      { lblData: "Current Bid : AED 52,000" },
+      { lblData: "Vehicle ID : VH-10234" },
+      { lblData: "Auction Date : 25 Dec 2025" },
+      { lblData: "Base Price : AED 45,000" },
+      { lblData: "Current Bid : AED 52,000" }
+    ];
+
+      for (var i = 1; i <= 16; i++) {
+    var details = this.view["details" + i];
+    if (details && details.segVehicleDetails) {
+      details.segVehicleDetails.setData(data);
+    }
+  }
+  },
+  
+  onRowClickAction: function (seg, sectionIndex, rowIndex) {
+  var rowData = seg.selectedRowItems[0];
+  var lblValue = rowData.lblData;
+ var details = seg.parent.parent;
+  details.txbData.text = lblValue;
+  details.flxSegment.setVisibility(false);
+    var transform = voltmx.ui.makeAffineTransform();
+  transform.rotate(0); 
+  details.imgarrow.transform = transform;
+}
+
+});
