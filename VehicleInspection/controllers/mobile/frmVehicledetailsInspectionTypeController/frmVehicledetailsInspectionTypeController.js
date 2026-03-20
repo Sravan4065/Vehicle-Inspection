2d5f6868a17dc7035f66c985b0a704dea84d2ea3
GDCIT-LAPT099\Dinesh_Bobbadhi <dinesh_bobbadhi@tecnics.com>
2026-03-20 22:29:20 +0530

    march20changes

--- VehicleInspection/controllers/mobile/frmVehicledetailsInspectionTypeController/frmVehicledetailsInspectionTypeController.js
+++ VehicleInspection/controllers/mobile/frmVehicledetailsInspectionTypeController/frmVehicledetailsInspectionTypeController.js
@@ -6,7 +6,7 @@ define({
   onPreShow: function(){
     toggleFooterIcons(this.view, "frmVehicledetailsInspectionType");
 //     this.clearData();
-    this.setDataToSeg();  
+    
     for (let i = 1; i <= 16; i++) {
   this.view["details" + i].flxArrow.onClick =
     this.toggleDetails.bind(this);
@@ -33,26 +33,203 @@ define({
   }
 },
 
-  setDataToSeg: function()
-  {
-    var data = [
-      { lblData: "Vehicle ID : VH-10234" },
-      { lblData: "Auction Date : 25 Dec 2025" },
-      { lblData: "Base Price : AED 45,000" },
-      { lblData: "Current Bid : AED 52,000" },
-      { lblData: "Vehicle ID : VH-10234" },
-      { lblData: "Auction Date : 25 Dec 2025" },
-      { lblData: "Base Price : AED 45,000" },
-      { lblData: "Current Bid : AED 52,000" }
-    ];
+//   setDataToSeg: function(response)
+//   {
+    
+//     this.view.details1.segVehicleDetails.widgetDataMap={
+//       "flxDropdown":"flxDropdown",
+//       "lblData":"lblData"
+//     };
+// //      var data = [];
+// //   var spec = response.data[0];
+    
+// //     var spec = [
+// //       { lblData: "Vehicle ID : VH-10234" },
+// //       { lblData: "Auction Date : 25 Dec 2025" },
+// //       { lblData: "Base Price : AED 45,000" },
+// //       { lblData: "Current Bid : AED 52,000" },
+// //       { lblData: "Vehicle ID : VH-10234" },
+// //       { lblData: "Auction Date : 25 Dec 2025" },
+// //       { lblData: "Base Price : AED 45,000" },
+// //       { lblData: "Current Bid : AED 52,000" }
+// //     ];
 
-      for (var i = 1; i <= 16; i++) {
-    var details = this.view["details" + i];
-    if (details && details.segVehicleDetails) {
-      details.segVehicleDetails.setData(data);
+// //       for (var i = 1; i <= 16; i++) {
+// //     var details = this.view["details" + i];
+// //     if (details && details.segVehicleDetails) {
+// //       details.segVehicleDetails.setData(data);
+// //     }
+// //   }
+    
+    
+//      var data = [];
+//   var spec = response.data[0];
+
+//   function pushData(title, arr) {
+//     if (arr && arr.length > 0) {
+//       for (var i = 0; i < arr.length; i++) {
+//         data.push({
+//           lblData: title + " : " + arr[i].value
+//         });
+//       }
+//     }
+//   }
+
+//   // Push required fields
+//   pushData("Year", spec.year_make);
+//   pushData("Technical Feature", spec.technical_features);
+//   pushData("Keys", spec.keys);
+//   pushData("Fuel Type", spec.fuel_type);
+//   pushData("Branch", spec.branch);
+//   pushData("Mileage Type", spec.mileage_type);
+//   pushData("Transmission", spec.transmission_type);
+//   pushData("Color", spec.color);
+//   pushData("Tyre Brand", spec.name);
+
+//   // Bind to segments
+//   for (var i = 1; i <= 16; i++) {
+//     var details = this.view["details" + i];
+//     if (details && details.segVehicleDetails) {
+//       details.segVehicleDetails.setData(data);
+//       this.view.details2.segVehicleDetails
+//       this.view.details3.segVehicleDetails
+//       this.view.details4.segVehicleDetails
+//       this.view.details5.segVehicleDetails
+//       this.view.details6.segVehicleDetails
+//       this.view.details7.segVehicleDetails
+//       this.view.details8.segVehicleDetails
+//       this.view.details9.segVehicleDetails
+//       this.view.details10.segVehicleDetails
+//       this.view.details11.segVehicleDetails
+//       this.view.details12.segVehicleDetails
+//       this.view.details13.segVehicleDetails
+//       this.view.details14.segVehicleDetails
+      
+//     }
+//   }
+
+    
+    
+    
+//   },
+  
+  
+  
+  setDataToSeg: function(response) {
+
+  var spec = response.data[0];
+
+  function formatData(title, arr) {
+    var result = [];
+    if (arr && arr.length > 0) {
+      for (var i = 0; i < arr.length; i++) {
+        result.push({
+          lblData: title + " : " + arr[i].value
+        });
+      }
     }
+    return result;
   }
-  },
+
+  // Assign data to each segment separately
+
+  if (this.view.details1.segVehicleDetails) {
+    this.view.details1.segVehicleDetails.setData(
+     formatData("Test","Test")
+    );
+  }
+
+  if (this.view.details2.segVehicleDetails) {
+    this.view.details2.segVehicleDetails.setData(
+      formatData("Test","Test")
+    );
+  }
+
+  if (this.view.details3.segVehicleDetails) {
+    this.view.details3.segVehicleDetails.setData(
+      formatData("Test","Test")
+    );
+  }
+
+  if (this.view.details4.segVehicleDetails) {
+    this.view.details4.segVehicleDetails.setData(
+   
+      
+      formatData("Color", spec.color)
+    );
+  }
+
+  if (this.view.details5.segVehicleDetails) {
+    this.view.details5.segVehicleDetails.setData(
+      formatData("Test","Test")
+    );
+  }
+
+  if (this.view.details6.segVehicleDetails) {
+    this.view.details6.segVehicleDetails.setData(
+   
+       formatData("Color", spec.color)
+    );
+  }
+
+  if (this.view.details7.segVehicleDetails) {
+    this.view.details7.segVehicleDetails.setData(
+   
+       formatData("Color", spec.color)
+    );
+  }
+
+  if (this.view.details8.segVehicleDetails) {
+    this.view.details8.segVehicleDetails.setData(
+      
+          formatData("Transmission", spec.transmission_type)
+    );
+  }
+
+  if (this.view.details9.segVehicleDetails) {
+    this.view.details9.segVehicleDetails.setData(
+      
+        formatData("Mileage Type", spec.mileage_type)
+    );
+  }
+
+      if (this.view.details10.segVehicleDetails) {
+    this.view.details10.segVehicleDetails.setData(
+      
+       formatData("Branch", spec.branch)
+    );
+  }
+
+       if (this.view.details11.segVehicleDetails) {
+    this.view.details11.segVehicleDetails.setData(
+      
+        formatData("Fuel Type", spec.fuel_type)
+    );
+  }
+       if (this.view.details12.segVehicleDetails) {
+    this.view.details12.segVehicleDetails.setData(
+      
+      formatData("Keys", spec.keys)
+    );
+  }
+       if (this.view.details13.segVehicleDetails) {
+    this.view.details13.segVehicleDetails.setData(
+   formatData("Test","Test")
+    );
+  }
+          if (this.view.details14.segVehicleDetails) {
+    this.view.details14.segVehicleDetails.setData(
+   formatData("Technical Feature", spec.technical_features)
+    );
+  }
+             if (this.view.details15.segVehicleDetails) {
+    this.view.details15.segVehicleDetails.setData(
+     formatData("Year", spec.year_make)
+    );
+  }
+    
+    
+},
   
   onRowClickAction: function (seg, sectionIndex, rowIndex) {
   var rowData = seg.selectedRowItems[0];
@@ -103,7 +280,9 @@ define({
   operationSuccessPending: function(response)
 {
   voltmx.application.dismissLoadingScreen();
+  
   voltmx.print(response);
+  this.setDataToSeg(response);  
 },
   operationFailurePending: function(error)
   {
