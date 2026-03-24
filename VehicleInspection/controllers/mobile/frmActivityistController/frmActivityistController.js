e533ae13a517fbf807288ed3bb632bdedc4abf33
GDCIT-LAPT099\Dinesh_Bobbadhi <dinesh_bobbadhi@tecnics.com>
2026-03-24 12:11:39 +0530

    committing mar24 changes.

--- VehicleInspection/controllers/mobile/frmActivityistController/frmActivityistController.js
+++ VehicleInspection/controllers/mobile/frmActivityistController/frmActivityistController.js
@@ -10,23 +10,58 @@ define({
     toggleFooterIcons(this.view, "frmActivityist");
     this.view.flxInspection.onClick = () =>
     {
+      if(voltmx.store.getItem("isLogin"))
+        {
       NavigationManager.push("frmMyInspectionsSummary");
+        }
+      else
+        {
+         NavigationManager.push("frmLogin"); 
+        }
     }
     this.view.flxIInward.onClick = () =>
     {
+      if(voltmx.store.getItem("isLogin"))
+        {
       NavigationManager.push("frmInwardEntrySummary");
+        }
+       else
+        {
+         NavigationManager.push("frmLogin"); 
+        }
     }
     this.view.flxWashing.onClick = () =>
     {
+      if(voltmx.store.getItem("isLogin"))
+        {
       NavigationManager.push("frmWashingSummary");
+        }
+       else
+        {
+         NavigationManager.push("frmLogin"); 
+        }
     }
     this.view.flxImages.onClick = () =>
     {
+      if(voltmx.store.getItem("isLogin"))
+        {
       NavigationManager.push("frmImagesSummary");
+        }
+       else
+        {
+         NavigationManager.push("frmLogin"); 
+        }
     }
     this.view.flxCompletedInspections.onClick = () =>
     {
+      if(voltmx.store.getItem("isLogin"))
+        {
       NavigationManager.push("frmCompletedInspections");
+        }
+       else
+        {
+         NavigationManager.push("frmLogin"); 
+        }
     }
   },
 
