0f2cc135feeba3afa518741ee40e1943714b6eb9
Sravan4065 <sravan_gunti@tecnics.com>
2026-04-15 16:36:57 +0530

    committing apr15 evng changes.

---
+++ VehicleInspection/studioactions/mobile/AS_FlexContainer_d2ce8f44967040d3afecdc9113229907.js
@@ -0,0 +1,4 @@
+function AS_FlexContainer_d2ce8f44967040d3afecdc9113229907(eventobject) {
+    var self = this;
+    return self.show.call(this);
+}
---
+++ VehicleInspection/studioactions/mobile/appEvents.js
@@ -0,0 +1,10 @@
+define({
+    /*
+        This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    AS_AppEvents_f2d02f07e4904651aadc95c53e2ae52e: function AS_AppEvents_f2d02f07e4904651aadc95c53e2ae52e(eventobject) {
+        var self = this;
+        voltmx.store.setItem("BASE_URL", "https://dev2-hcltx.et.ae:443/");
+        fetchClientProperties.call(this);
+    }
+});
